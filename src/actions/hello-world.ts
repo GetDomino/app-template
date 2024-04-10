bee01e5a5b95426f7b160916a76f5d05071eb95d
Peter Szoke <ps@jpegs.ro>
2024-04-10 18:36:48 +0200

    feat: update template to match new sdk

--- src/actions/hello-world.ts
+++ src/actions/hello-world.ts
@@ -1,4 +1,7 @@
-export default function() {
+import { IRunContext } from "@domino-sdk/core";
+
+export default function(context: IRunContext) {
+  // context.input is the input data passed to the action
   return {
     result: 'Hello world!'
   }
